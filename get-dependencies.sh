caae695f785c4c3fec942f30722a637b5b8c6cab
Jamie Rodriguez <jamie@Jamies-MacBook-Pro.local>
2022-08-19 19:05:49 +0800

    Refactor get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Unity: unit testing framework
+# TO-DO: Consider extracting only the C source code, there's a lot of build
+# config files contained in the releases...
+
+RELEASE_TARBALL=v2.5.2.tar.gz
+
 cd lib/
-curl -O --location https://github.com/ThrowTheSwitch/Unity/archive/refs/tags/v2.5.2.tar.gz
-tar -zxf v2.5.2.tar.gz
-rm v2.5.2.tar.gz
+curl --remote-name --location https://github.com/ThrowTheSwitch/Unity/archive/refs/tags/${RELEASE_TARBALL}
+tar --gunzip --extract --file $RELEASE_TARBALL
+# Need to remove the .git/ folder that the Unity v2.5.2 tarball leaves behind;
+# - could cause problems if other external libraries are added that also
+# contain a .git/ folder
+rm -rf .git/ $RELEASE_TARBALL
